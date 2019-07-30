86a6dc2e334dacd66761a49940e9ec478f7668a7
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-07-30 20:24:44 +0530

    good question

--- InterviewBit/PrimeSum.cpp
+++ InterviewBit/PrimeSum.cpp
@@ -1,24 +1,18 @@
-vector<int> Solution::primesum(int n) {
-    vector<bool> isPrime;
-    for(int i = 0; i < n; i++) {
-        isPrime.push_back(true);
-    }
-    for(int p = 2; p <= sqrt(n); p++) {
-        if(isPrime[p]) {
-            // Update all primes of p
-            for(int i = 2; p * i <= n; i++) {
-                isPrime[p * i] = false;
+vector<int> Solution::primesum(int A) {
+    vector<bool> prime(A + 1, true);
+    prime[0] = prime[1] = false;
+    for(int i = 2; i <= A; i++) {
+        if(prime[i]) {
+            for(int j = 2 * i; j <= A; j += i) {
+                prime[j] = false;
             }
         }
     }
-    vector<int> res;
-    for(int i = 2; i < n; i++) {
-        if(isPrime[i] && isPrime[n - i]) {
-            res.push_back(i);
-            res.push_back(n - i);
-            break;
-        }
+    for(int i = 2; i < A; i++) {
+        if(prime[i] && prime[A - i])
+            return { i, A - i };
     }
-    return res;
+    return {};
 }
+
 
