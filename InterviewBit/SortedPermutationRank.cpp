8e96399701c13bf4203a9562b6b33f44e7d0b98e
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-07-31 12:18:38 +0530

    important

--- InterviewBit/SortedPermutationRank.cpp
+++ InterviewBit/SortedPermutationRank.cpp
@@ -1,28 +1,26 @@
-#define MOD 1000003
 #define ll long long int
+#define MOD 1000003
 
-ll findSmallerInRight(string s, int l, int h) {
-    int cntRight = 0;
-    for(int i = l + 1; i <= h; i++) {
-        if(s[i] < s[l])
-            cntRight++;
+ll countRight(string s, int i) {
+    ll c = 0;
+    for(int j = i + 1; j < s.size(); j++) {
+        if(s[j] < s[i])
+            c++;
     }
-    return cntRight;
+    return c;
 }
 
 int Solution::findRank(string A) {
-    ll n = A.size();
-    int fact[n + 1];
-    fact[0] = 1;
+    ll n = A.size(), ret = 0;
+    ll f[n + 1];
+    f[0] = 1;
     for(int i = 1; i <= n; i++) {
-        fact[i]  = ((fact[i - 1]) * (i) )%MOD;
+        f[i] = (f[i - 1] % MOD * i % MOD) % MOD;
     }
-    ll rank = 0;
-    for(int i = 0; i < A.size(); i++) {
-        ll mul = fact[n - i - 1];
-        ll cntRight = findSmallerInRight(A, i, n - 1);
-        rank = (rank % MOD + (cntRight % MOD * mul % MOD) % MOD) % MOD;
+    for(int i = 0; i < n; i++) {
+        ll cnt  = countRight(A, i), fact = f[n - i - 1];
+        ret = ret % MOD + (cnt % MOD * fact % MOD) % MOD;
     }
-    return (int)(rank + 1);
+    return int((ret + 1));
 }
 
