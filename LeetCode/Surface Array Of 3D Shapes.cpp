8b467d5bb02c73f4ee31a6f50543962b93b0ffe4
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-02-01 12:09:03 +0530

    IMP revised

--- LeetCode/Surface Array Of 3D Shapes.cpp
+++ LeetCode/Surface Array Of 3D Shapes.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
-    vector<int> dx = { 0, 0, -1, 1 };
-    vector<int> dy = { 1, -1, 0, 0 };
-    
     int surfaceArea(vector<vector<int>>& grid) {
+        vector<int> dx = { -1, 1, 0, 0 };
+        vector<int> dy = { 0, 0, -1, 1 };
         int ret = 0;
         int n = grid.size(), m = grid[0].size();
         for(int i = 0; i < n; i++) {
@@ -11,12 +10,11 @@ public:
                 if(!grid[i][j])
                     continue;
                 ret += 2;
-                int val = grid[i][j];
                 for(int k = 0; k < 4; k++) {
-                    int nx = i + dx[k], ny = j + dy[k], neigh = 0;
-                    if(nx >= 0 && ny >= 0 && nx < n && ny < m)
-                            neigh = grid[nx][ny];
-                    ret += max(val - neigh, 0);
+                    int curri = i + dx[k], currj = j + dy[k], neigh = 0;
+                    if(curri >= 0 && curri < n && currj >= 0 && currj < m)
+                        neigh = grid[curri][currj];
+                    ret += max(0, grid[i][j] - neigh);
                 }
             }
         }
