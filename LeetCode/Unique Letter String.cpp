f232d35338f15a2e6968503ba189ff8b5a3df0b3
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-02-08 19:44:39 +0530

    IMP revised

--- LeetCode/Unique Letter String.cpp
+++ LeetCode/Unique Letter String.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    int uniqueLetterString(string S) {
-        int n = S.size();
-        vector<int> prev(n, -1);
-        vector<int> next(n, n);
-        vector<int> last(26, -1);
-        for(int i = 0; i < n; i++) {
-            int c = S[i] - 'A';
-            prev[i] = last[c];
-            if(last[c] != -1)
-                next[last[c]] = i;
-            last[c] = i;
+    int uniqueLetterString(string s) {
+        int n = s.size();
+        vector<int> prev(n, -1), next(n, n);
+        vector<int> pos(26, -1);
+        for(int i = 0; i < s.size(); i++) {
+            prev[i] = pos[s[i] - 'A'];
+            if(pos[s[i] - 'A'] != -1)
+                next[pos[s[i] - 'A']] = i;
+            pos[s[i] - 'A'] = i;
         }
         int ans = 0;
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < s.size(); i++) {
             ans += (i - prev[i]) * (next[i] - i);
         }
         return ans;
